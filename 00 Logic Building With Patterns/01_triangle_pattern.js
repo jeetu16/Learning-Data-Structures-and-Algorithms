f8471364ff43db13cab206678dfb87f0062794d3
Jeetu Dewangan <jeetukumar.jk367@gmail.com>
2024-12-15 22:31:35 +0530

    solved reverse Triangle Pattern1

--- 00 Logic Building With Patterns/01_triangle_pattern.js
+++ 00 Logic Building With Patterns/01_triangle_pattern.js
@@ -54,7 +54,26 @@ function trianglePattern3(num) {
 // 4 5 6
 // 7 8 9 10
 
+function reverseTrianglePattern1(input) {
+    let pattern = ""
+    for (let i = input; i >= 0; i--) {
+        for (let j = 1; j <= i; j++) {
+            pattern += `${j} `;
+        }
+        pattern += "\n";
+    }
+    
+    return pattern;
+}
+// Output :
+// 1 2 3 4 5
+// 1 2 3 4
+// 1 2 3
+// 1 2
+// 1
+
 
 console.log(trianglePattern1(5));
 console.log(trianglePattern2(5));
 console.log(trianglePattern3(5));
+console.log(reverseTrianglePattern1(5));
