ed68e7d1c9f88ac3468ea109acffa7575ea3bad6
Jeetu Dewangan <jeetukumar.jk367@gmail.com>
2024-12-16 23:35:00 +0530

    solved numberPyramidPattern1 problem

--- 00 Logic Building With Patterns/01_triangle_pattern.js
+++ 00 Logic Building With Patterns/01_triangle_pattern.js
@@ -72,8 +72,31 @@ function reverseTrianglePattern1(input) {
 // 1 2
 // 1
 
+function numberPyramidPattern1(input) {
+    let pattern = '';
+
+    for (let i = 1; i <= input; i++) {
+        for (let k = input-i; k >= 0; k--) {
+            pattern += "  ";
+        }
+        for (let j = 1; j <= 2*i-1 ; j++) {
+            pattern += `${j} `; 
+        }
+        pattern += '\n';
+    }
+
+    return pattern
+}
+
+// Output :
+//         1
+//       1 2 3
+//     1 2 3 4 5
+//   1 2 3 4 5 6 7
+// 1 2 3 4 5 6 7 8 9
 
 console.log(trianglePattern1(5));
 console.log(trianglePattern2(5));
 console.log(trianglePattern3(5));
 console.log(reverseTrianglePattern1(5));
+console.log(numberPyramidPattern1(5));
